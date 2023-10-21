4b159970efe6e579faa899ca5922cc440fbb0b00
anas jaidi <anas.jaidi@icloud.com>
2023-10-21 18:26:14 +0100

    feat: integrate io-types into client

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,14 +1,14 @@
 import { Title } from "ui";
 import _styles from "./page.module.css";
 import Image from "next/image";
-import img from 'assets-workspace/images/test.jpeg'
+import img from "assets-workspace/images/test.jpeg";
 
 export default function HomePage(): JSX.Element {
   return (
     <>
       <Title />
       <main className="bg-red-500">HomePage</main>
-      <Image src={img} alt="testing asstets image"/>
+      <Image src={img} alt="testing asstets image" />
     </>
   );
 }
