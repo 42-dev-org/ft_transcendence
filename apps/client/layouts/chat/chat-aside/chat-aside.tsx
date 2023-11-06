c9e80706240d8e85f43a0108cc8d80fbd90e9b3d
anas jaidi <anas.jaidi@icloud.com>
2023-11-06 19:57:24 +0100

    chore: chat aside layout

---
+++ apps/client/layouts/chat/chat-aside/chat-aside.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './chat-aside.module.css'
+
+export default function ChatAside(): JSX.Element {
+  return (
+    <div className={`${styles['chat-aside-container']}`}>ChatAside</div>
+  )
+}
