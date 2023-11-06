c9788abe5b9dc83baeb60d4cf1b5077d074def60
mustapha ouarsass <hzan1996@gmail.com>
2023-11-06 23:37:20 +0100

    update chat main layout

--- apps/client/layouts/chat/chat-main/chat-main.tsx
+++ apps/client/layouts/chat/chat-main/chat-main.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import profileImage from "assets-workspace/images/profile.png";
 import ChatMainNav from "../../../components/chat/chat-main-nav/chat-main-nav";
 import ChatMainMessagesContainer from "../../../components/chat/chat-main-messages-container/chat-main-messages-container";
 import ChatMainSendMessage from "../../../components/chat/chat-main-send-message/chat-main-send-message";
 import styles from "./chat-main.module.css";
 
+const user = {
+  name: "kristof ouarsas",
+  username: "ouarsas",
+  image: profileImage,
+  status: "online" as const,
+};
+
 export default function ChatMain(): JSX.Element {
   return (
     <div className={`${styles["chat-main-container"]}`}>
-      <ChatMainNav />
+      <ChatMainNav {...user} />
       <ChatMainMessagesContainer />
       <ChatMainSendMessage />
     </div>
