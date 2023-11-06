d68ab29db2575f9df4496b8d182db5f419067859
mustapha ouarsass <hzan1996@gmail.com>
2023-11-06 23:37:06 +0100

    update chat aside layout

--- apps/client/layouts/chat/chat-aside/chat-aside.tsx
+++ apps/client/layouts/chat/chat-aside/chat-aside.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
-import styles from './chat-aside.module.css'
+import React from "react";
+import profileImage from "assets-workspace/images/profile.png";
+import ChatAsideSearch from "../../../components/chat/chat-aside-search/chat-aside-search";
+import ChatAsideContacts from "../../../components/chat/chat-aside-contacts/chat-aside-contacts";
+import styles from "./chat-aside.module.css";
+
+const contacts = [
+  { name: "anas", lastMessage: "hi", image: profileImage, key: Math.random().toString() },
+  { name: "anas", lastMessage: "hi", image: profileImage, key: Math.random().toString() },
+  { name: "anas", lastMessage: "hi", image: profileImage, key: Math.random().toString() },
+  { name: "anas", lastMessage: "hi", image: profileImage, key: Math.random().toString() },
+  { name: "anas", lastMessage: "hi", image: profileImage, key: Math.random().toString() },
+  { name: "anas", lastMessage: "hi", image: profileImage, key: Math.random().toString() },
+  { name: "anas", lastMessage: "hi", image: profileImage, key: Math.random().toString() },
+  { name: "anas", lastMessage: "hi", image: profileImage, key: Math.random().toString() },
+  { name: "anas", lastMessage: "hi", image: profileImage, key: Math.random().toString() },
+  { name: "anas", lastMessage: "hi", image: profileImage, key: Math.random().toString() },
+  { name: "anas", lastMessage: "hi", image: profileImage, key: Math.random().toString() },
+  { name: "anas", lastMessage: "hi", image: profileImage, key: Math.random().toString() },
+  { name: "anas", lastMessage: "hi", image: profileImage, key: Math.random().toString() },
+];
 
 export default function ChatAside(): JSX.Element {
   return (
-    <div className={`${styles['chat-aside-container']}`}>ChatAside</div>
-  )
+    <div className={`${styles["chat-aside-container"]}`}>
+      <ChatAsideSearch />
+      <ChatAsideContacts contacts={contacts} />
+    </div>
+  );
 }
