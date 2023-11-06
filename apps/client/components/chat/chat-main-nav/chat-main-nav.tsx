bbfb5d92b3d0e275fc79356073a8d696f88829e5
mustapha ouarsass <hzan1996@gmail.com>
2023-11-06 23:36:18 +0100

    add chat main nav section

--- apps/client/components/chat/chat-main-nav/chat-main-nav.tsx
+++ apps/client/components/chat/chat-main-nav/chat-main-nav.tsx
@@ -1,8 +1,54 @@
-import React from 'react'
-import styles from './chat-main-nav.module.css'
+"use client";
+import Image from "next/image";
+import type { StaticImageData } from "next/image";
+import ChatOptionsArrow from "../chat-options-arrow/chat-options-arrow";
+import styles from "./chat-main-nav.module.css";
 
-export default function ChatMainNav(): JSX.Element {
+interface PropsType {
+  name: string;
+  username: string;
+  image: string | StaticImageData;
+  status: "online" | "offline";
+}
+
+const links = [
+  {name: 'delete', key: Math.random().toString()},
+  {name: 'fuck', key: Math.random().toString()},
+  {name: 'awdy', key: Math.random().toString()},
+]
+
+
+export default function ChatMainNav({
+  username,
+  name,
+  image,
+  status,
+}: PropsType): JSX.Element {
+  
+
   return (
-    <div className={`${styles['chat-nav-container']}`}>ChatMainNav</div>
-  )
+    <div className={`${styles["chat-nav-container"]}`}>
+      <div className="flex gap-3">
+        <div className="max-w-16 max-h-16 rounded-full overflow-hidden my-auto">
+          <Image alt="profile" src={image} />
+        </div>
+        <div className="flex flex-col">
+          <p className="font-semibold text-xl capitalize text-white tracking-wider">
+            {name}
+          </p>
+          <p className="text-xs opacity-70 capitalize text-[#FFF] font-light">
+            {username}
+          </p>
+          <p
+            className={`${
+              status === "online" ? "text-green-400" : "text-red"
+            } text-xs font-medium`}
+          >
+            {status}
+          </p>
+        </div>
+      </div>
+      <ChatOptionsArrow links={links}/>
+    </div>
+  );
 }
