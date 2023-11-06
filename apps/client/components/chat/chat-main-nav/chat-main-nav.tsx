a5f392a699ae0710f44631b63c65cc29695971b8
anas jaidi <anas.jaidi@icloud.com>
2023-11-06 19:58:18 +0100

    chore: add nav chat section

---
+++ apps/client/components/chat/chat-main-nav/chat-main-nav.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './chat-main-nav.module.css'
+
+export default function ChatMainNav(): JSX.Element {
+  return (
+    <div className={`${styles['chat-nav-container']}`}>ChatMainNav</div>
+  )
+}
