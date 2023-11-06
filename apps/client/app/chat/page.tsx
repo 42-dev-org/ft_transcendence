70045d2f884bfddace9dfa7d5bd59460e18876a9
anas jaidi <anas.jaidi@icloud.com>
2023-11-06 19:56:39 +0100

    chore: add chat page

---
+++ apps/client/app/chat/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ChatContainer from '../../containers/chat/chat-container'
+
+export default function ChatPage(): JSX.Element {
+  return (
+    <ChatContainer />
+  )
+}
