08b261616df9ab5d0d2580df0e385c3f566c1207
mustapha ouarsass <hzan1996@gmail.com>
2023-11-08 19:02:41 +0100

    change the value

--- apps/client/layouts/chat/chat-main/chat-main.tsx
+++ apps/client/layouts/chat/chat-main/chat-main.tsx
@@ -6,8 +6,8 @@ import ChatMainSendMessage from "../../../components/chat/chat-main-send-message
 import styles from "./chat-main.module.css";
 
 const user = {
-  name: "kristof ouarsas",
-  username: "ouarsas",
+  name: "cristoph ouarsass",
+  username: "ouarsass",
   image: profileImage,
   status: "online" as const,
 };
