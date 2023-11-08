54ac133df8123adde2e5199efc718fe8821a79be
mustapha ouarsass <hzan1996@gmail.com>
2023-11-08 19:03:53 +0100

    inpt send msg & submite

--- apps/client/components/chat/chat-main-send-message/chat-main-send-message.tsx
+++ apps/client/components/chat/chat-main-send-message/chat-main-send-message.tsx
@@ -2,9 +2,21 @@ import React from "react";
 import styles from "./chat-main-send-message.module.css";
 
 export default function ChatMainSendMessage(): JSX.Element {
+
+
+
+
   return (
     <div className={`${styles["chat-send-message-container"]}`}>
-      ChatMainSendMessage
+    	<div  >
+			<form >
+			    <input className="w-[80%] rounded-2xl py-1 px-10 bg-[#837979]" type="text" />
+			    <button className="text-white " type="submit">
+					Started
+				</button>
+			</form>
+    	</div>
+      	ChatMainSendMessage
     </div>
   );
 }
