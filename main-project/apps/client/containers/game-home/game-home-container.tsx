5977681f79aae4acb961a52afd52f66f3a9d5ad6
mustapha ouarsass <hzan1996@gmail.com>
2023-11-12 22:15:40 +0100

    center conponents

--- main-project/apps/client/containers/game-home/game-home-container.tsx
+++ main-project/apps/client/containers/game-home/game-home-container.tsx
@@ -3,7 +3,7 @@ import HomeGameMain from "../../layouts/game-home-main/game-home-main";
 
 export default function GameHomeContainer() {
   return (
-    <div className="w-full h-screen bg-black flex justify-center">
+    <div className="w-full h-screen bg-black flex justify-center items-center">
       <HomeGameMain />
     </div>
   );
