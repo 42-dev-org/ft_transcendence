c7ebbda1ba57b10848ac860465dc8ec3620c4422
mustapha ouarsass <hzan1996@gmail.com>
2023-11-12 20:43:05 +0100

    add singl game layouts

---
+++ main-project/apps/client/layouts/single-game/single-game-header/single-game-header.tsx
@@ -0,0 +1,39 @@
+import type { StaticImageData } from "next/image";
+import SingleGamePlayerCard from "../../../components/single-game/single-game-player-card/single-game-player-card";
+import SingleGameResult from "../../../components/single-game/single-game-result/single-game-result";
+
+interface Player {
+  username: string;
+  fullName: string;
+  image: string | StaticImageData;
+  score: number;
+}
+
+interface PropsType {
+  player1: Player;
+  player2: Player;
+}
+
+export default function SingleGameHeader({
+  player1,
+  player2,
+}: PropsType): JSX.Element {
+  return (
+    <div className="bg-[#1B1B1B] w-full lg:w-3/4 px-4 py-4 flex justify-between  items-center border rounded-lg">
+      <SingleGamePlayerCard
+        direction="ltr"
+        fullName={player1.fullName}
+        image={player1.image}
+        username={player1.username}
+      />
+      <SingleGameResult p1={player1.score} p2={player2.score} />
+
+      <SingleGamePlayerCard
+        direction="rtl"
+        fullName={player2.fullName}
+        image={player2.image}
+        username={player2.username}
+      />
+    </div>
+  );
+}
---
+++ main-project/apps/client/layouts/single-game/single-game-main/single-game-main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SinglegameMain(): JSX.Element {
+  return (
+    <div className=" w-full lg:w-3/4 h-[1024px] p-8">
+      <div className="game-canvas bg-[#40554f] w-full h-full">anas</div>
+    </div>
+  );
+}
