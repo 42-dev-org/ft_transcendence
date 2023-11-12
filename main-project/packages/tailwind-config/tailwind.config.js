058591a025f9b19d85698564db3e105c1ed3a5d0
Anas jaidi <anas.jaidi@icloud.com>
2023-11-12 02:48:49 +0100

    chore: repo settings

--- main-project/packages/tailwind-config/tailwind.config.js
+++ main-project/packages/tailwind-config/tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./containers/**/*.{js,ts,jsx,tsx,mdx}",
+    "./layouts/**/*.{js,ts,jsx,tsx,mdx}",
     "../../packages/ui/**/*.{tsx, ts}",
  
     // Or if using `src` directory:
