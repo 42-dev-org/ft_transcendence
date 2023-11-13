8a2c9fdbb3da7ad7ec74b239916d3f532d535ddc
mustapha ouarsass <hzan1996@gmail.com>
2023-11-13 22:55:09 +0100

    add gif ping pong

---
+++ main-project/apps/client/components/pong/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './index.css'
+
+function Pong() {
+  return (
+    <div className="field">
+      <div className="net"></div>
+      <div className="ping"></div>
+      <div className="pong"></div>
+      <div className="ball"></div>
+    </div>
+  );
+}
+
+export default Pong;
