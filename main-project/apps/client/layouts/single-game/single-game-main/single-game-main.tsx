df26bc810a32a3e9755bc4cca65692c4fe8887d0
mustapha ouarsass <hzan1996@gmail.com>
2023-11-15 20:06:50 +0100

    resize the page Game

--- main-project/apps/client/layouts/single-game/single-game-main/single-game-main.tsx
+++ main-project/apps/client/layouts/single-game/single-game-main/single-game-main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SinglegameMain(): JSX.Element {
   return (
-    <div className=" w-full lg:w-3/4 h-[1024px] p-8">
+    <div className="w-full h-[1024px] p-8">
       <div className="game-canvas bg-[#40554f] w-full h-full">anas</div>
     </div>
   );
