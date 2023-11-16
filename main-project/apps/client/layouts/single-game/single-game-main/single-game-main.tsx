e391fa8806ab31a8c350ddb96f5cb6d1e566b733
mustapha ouarsass <hzan1996@gmail.com>
2023-11-16 20:50:07 +0100

    add popup GameOver

--- main-project/apps/client/layouts/single-game/single-game-main/single-game-main.tsx
+++ main-project/apps/client/layouts/single-game/single-game-main/single-game-main.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 
-export default function SinglegameMain(): JSX.Element {
+export default function SinglegameMain({isActive} : {isActive: boolean}): JSX.Element {
+
+
+
   return (
-    <div className="w-full h-[1024px] p-8">
-      <div className="game-canvas bg-[#40554f] w-full h-full">anas</div>
+    <div className=" w-full h-[1024px] p-8">
+      <div className="game-canvas  bg-[#c2c6c5] w-full h-full relative">
+        xxxxx
+          {isActive && 
+          <div className="absolute  top-0 flex justify-center items-center w-full h-full bg-[rgba(0,0,0,0.1)]">
+            <div className=" w-3/4 z-50 h-3/4 flex flex-col py-5 items-center bg-black rounded-lg text-white">
+              GAME OVER
+                <div className="w-2/3 flex flex-row h-2/3  items-center text-white">
+                    here
+                </div>
+            </div> 
+            </div>
+          }
+      </div>
     </div>
   );
 }
