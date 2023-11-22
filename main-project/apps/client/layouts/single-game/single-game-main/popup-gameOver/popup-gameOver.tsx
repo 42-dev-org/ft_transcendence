263d69abcbd07cb273d5f15a139c0a52c7195189
mustapha ouarsass <hzan1996@gmail.com>
2023-11-22 19:20:24 +0100

    updated

--- main-project/apps/client/layouts/single-game/single-game-main/popup-gameOver/popup-gameOver.tsx
+++ main-project/apps/client/layouts/single-game/single-game-main/popup-gameOver/popup-gameOver.tsx
@@ -1,23 +1,62 @@
-import React from 'react'
+// components/PopupGameOver
+import React from "react";
 import Image from "next/image";
 import gameover from "assets-workspace/images/GameOver.gif";
 import { TbReload } from "react-icons/tb";
 import { FaHome } from "react-icons/fa";
-import SingleGameMainButton from '../single-game-main-button/single-game-main-button';
+import SingleGameMainButton from "../single-game-main-button/single-game-main-button";
+import Confetti from "./popup-confetti/popup-confetti";
 
-export default function PopupGameOver() {
+export function PopupGameOver() {
   return (
-    <div className="absolute w-full h-full backdrop-blur-sm top-0 flex justify-center items-center ">
-            <div className=" flex justify-center items-center w-full">
-              <div className=" w-3/4 z-50 h-3/4 flex flex-col py-6 items-center backdrop-blur-2xl bg-white bg-opacity-25 rounded-lg text-white ">
-                <Image alt="gameGif" height={200} src={gameover} width={400} />
-                <div className="w-[80%] flex flex-col md:flex-row h-full items-center text-white text-sm md:justify-between gap-6">
-
-                  <SingleGameMainButton title='PLAY AGAIN' link='#' ButtonIcon={TbReload} />
-                  <SingleGameMainButton title='BACK HOME' link='/profile' ButtonIcon={FaHome} />
-                </div>
-              </div>
-            </div>
+      <div className="absolute w-full h-full backdrop-blur-sm top-0 flex justify-center items-center ">
+      <div className=" flex justify-center items-center w-full">
+        <div className=" w-3/4 z-50 h-3/4 flex flex-col py-6 items-center backdrop-blur-2xl bg-white bg-opacity-25 rounded-lg text-white ">
+          <Image alt="gameGif" height={200} src={gameover} width={400} />
+          <div className="w-[80%] flex flex-col md:flex-row h-full items-center text-white text-sm md:justify-between gap-6">
+            {/* Add the Confetti component here */}
+            <Confetti />
+            <SingleGameMainButton
+              title="PLAY AGAIN"
+              link="#"
+              ButtonIcon={TbReload}
+              />
+            <SingleGameMainButton
+              title="BACK HOME"
+              link="/profile"
+              ButtonIcon={FaHome}
+              />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+
+
+
+
+// import React from 'react'
+// import Image from "next/image";
+// import gameover from "assets-workspace/images/GameOver.gif";
+// import { TbReload } from "react-icons/tb";
+// import { FaHome } from "react-icons/fa";
+// import SingleGameMainButton from '../single-game-main-button/single-game-main-button';
+  
+// export default function PopupGameOver() {
+//   return (
+//     <div className="absolute w-full h-full backdrop-blur-sm top-0 flex justify-center items-center ">
+//             <div className=" flex justify-center items-center w-full">
+//               <div className=" w-3/4 z-50 h-3/4 flex flex-col py-6 items-center backdrop-blur-2xl bg-white bg-opacity-25 rounded-lg text-white ">
+//                 <Image alt="gameGif" height={200} src={gameover} width={400} />
+//                 <div className="w-[80%] flex flex-col md:flex-row h-full items-center text-white text-sm md:justify-between gap-6">
+
+//                   <SingleGameMainButton title='PLAY AGAIN' link='#' ButtonIcon={TbReload} />
+//                   <SingleGameMainButton title='BACK HOME' link='/profile' ButtonIcon={FaHome} />
+//                 </div>
+//               </div>
+//             </div>
+//     </div>
+//   );
+// }
--- main-project/apps/client/layouts/single-game/single-game-main/single-game-main.tsx
+++ main-project/apps/client/layouts/single-game/single-game-main/single-game-main.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 
-import PopupGameOver from "./popup-gameOver/popup-gameOver";
+import { PopupGameOver } from "./popup-gameOver/popup-gameOver";
 
-
-export default function SinglegameMain({isActive}: {isActive: boolean}): JSX.Element {
-
+export default function SinglegameMain({
+  isActive,
+}: {
+  isActive: boolean;
+}): JSX.Element {
   return (
     <div className=" w-full h-[1024px] p-8">
       <div className="game-canvas  bg-blue-950 w-full h-full relative border-[#B2F35F] border-[5px] flex flex-col justify-between  py-2 ">
-            <div className="w-full h-4 flex justify-center items-center">
-              <div className="w-20 h-full bg-white"></div>
-            </div>
-            <div className="w-full h-2 flex justify-center items-center">
-              <div className="w-full h-1/3 bg-[#B2F35F]"></div>
-            </div>
-            <div className="w-full h-4 flex justify-center items-center">
-              <div className="w-20 h-full bg-white"></div>
-            </div>
-        {isActive && (
-          <PopupGameOver/>
-        )}
+        <div className="w-full h-4 flex justify-center items-center">
+          <div className="w-20 h-full bg-white"></div>
+        </div>
+        <div className="w-full h-2 flex justify-center items-center">
+          <div className="w-full h-1/3 bg-[#B2F35F]"></div>
+        </div>
+        <div className="w-full h-4 flex justify-center items-center">
+          <div className="w-20 h-full bg-white"></div>
+        </div>
+        {isActive && <PopupGameOver />}
       </div>
     </div>
   );
