87783c73d694658857406d694894c2f407aaa937
zakaria maziane <ziko98greenois@gmail.com>
2023-11-23 21:17:47 +0100

    feat: search chat

--- main-project/apps/client/app/(menu-aside-routers)/chat/page.tsx
+++ main-project/apps/client/app/(menu-aside-routers)/chat/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Chat = () => {
   return (
-    <div>Chat</div>
+    <div className=' flex  w-full h-full bg-slate-400 text-white'>
+      <div className='flex w-1/3 justify-center  bg-red-950 h-full py-2 px-1'>
+        <input type="text" className=' h-7 ' />
+      </div>
+      <div></div>
+    </div>
   )
 }
 
