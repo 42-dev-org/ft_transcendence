7c7bf7e4bce646fa4792e7524dfc6cadec2d537f
zakaria maziane <ziko98greenois@gmail.com>
2023-12-05 20:10:59 +0100

    feat: modal for creat direct  conversation and creat channels

---
+++ main-project/apps/client/components/liste/ListUsersChat.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import React from 'react'
+import Button from '../Button'
+
+
+type ListUsersChatProps = {
+    name: string,
+    url: string
+    className?: string
+}
+
+export function ListUsersChat({name,  url, className}: ListUsersChatProps)  {
+  return (
+    <div className={`flex flex-row justify-between w-72 rounded-lg bg-black  my-1 h-9 items-center ${className} `} >
+        <Image width={35} height={35} className='rounded-full flex items-center ml-1 w-8 h-8' src={url} alt={name} />
+        <span className='flex whitespace-nowrap items-center text-white'>
+            {name}
+        </span>
+        <Button title='add' onClick={() => {}} className='w-11 h-8 mr-1 flex justify-center items-center' />
+
+    </div>
+  )
+}
