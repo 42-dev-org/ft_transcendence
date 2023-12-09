30a401cbae797ae99e6f37e18419b7ff2c16791c
XasTrO <team.abn33@gmail.com>
2023-12-09 19:25:05 +0200

    dobe

--- main-project/apps/client/components/game-home/invite-friend/invite-friend.tsx
+++ main-project/apps/client/components/game-home/invite-friend/invite-friend.tsx
@@ -1,49 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { BiSearchAlt } from "react-icons/bi";
 import FriendItem from "./friend-item";
-import SearchBar from "./search-bar";
 import { friends } from "./data/friends";
 import { user } from "./interface/user";
+import SearchBar from "./search-bar";
 
 const InviteFriend = () => {
-    const [end, setEnd] = useState<number>(10)
-    const [friendsData, setFriendsData] = useState<user[]>(friends.slice(0, end));
+  const [end, setEnd] = useState<number>(10);
+  const [friendsData, setFriendsData] = useState<user[]>(friends.slice(0, end));
+
+
     
-    // useEffect(() => {
-    //     console.log(end);
-    //     setFriendsData(friends.slice(0, end))
-    // }, [end])
-
-
-    const handleMore = ()=> {
-        setEnd(end + 10)
-    }
+    const handleMore = () => {
+      setFriendsData(friends.slice(0, end + 10));
+      setEnd(end + 10);
+  };
 
   return (
-        <div className="container  h-full w-full flex justify-center items-center">
-          <div className="flex w-full flex-col justify-center items-center px-4 gap-8 pt-7 ">
-            <div className="w-full ">
-              <SearchBar setFriendsData={setFriendsData} end={end} setEnd={setEnd}/>
-              <ul className=" w-full flex flex-col justify-start items-center pt-10 gap-4">
-                {friendsData.map((friend : user) => (
-                  <FriendItem
-                    key={friend.id}
-                    fullName={friend.fullName}
-                    username={friend.username}
-                    avatar={friend.avatar}
-                    id={friend.id}
-                  />
-                ))}
-              </ul>
-            </div>
-              <button className="bg-black w-full py-3 mt-4 rounded-lg text-white flex justify-center items-center" onClick={handleMore}>
-                    More
-              </button>
-          </div>
+    <div className="container  h-full w-full flex justify-center items-center">
+      <div className="flex w-full flex-col justify-center items-center px-4 gap-8 pt-7 ">
+        <div className="w-full ">
+          <SearchBar setFriendsData={setFriendsData} setEnd={setEnd} end={end}/>
+          <ul className=" w-full flex flex-col justify-start items-center pt-10 gap-4">
+            {friendsData.map((friend: user) => (
+              <FriendItem
+                key={friend.id}
+                fullName={friend.fullName}
+                username={friend.username}
+                avatar={friend.avatar}
+                id={friend.id}
+              />
+            ))}
+          </ul>
         </div>
+        <button className="bg-black w-full py-3 mt-4 rounded-lg text-white flex justify-center items-center" onClick={handleMore}>
+          More
+        </button>
+      </div>
+    </div>
   );
-
-
 };
 
 export default InviteFriend;
--- main-project/apps/client/components/game-home/invite-friend/search-bar.tsx
+++ main-project/apps/client/components/game-home/invite-friend/search-bar.tsx
@@ -6,56 +6,71 @@ import { friends } from "./data/friends";
 import { user } from "./interface/user";
 
 const SearchBar = ({setFriendsData, end, setEnd} ) => {
-const [search, setSearch] = useState<string>("")
+  const [search, setSearch] = useState<string>("")
 
 
-const handelOnClick = () =>  {
+  const handleOnClick = async () => {
+    const currentSearch = search; // Capture the current value of the search state
   
-  const fetchData = async () => {
-
-  const res = await fetch('http://localhost:3001/api/search?key='+search, {
-    method: "GET",
-  });
-  const result = await res.json();
-  setEnd(10);
-  setFriendsData(result.slice(0, end))
-  }
-  fetchData();
-}
-
-
-    const handleChange =  (e: any) =>
-    {
-        setSearch(e.target.value);
-        
-        // if(e.target.value == ""){
-        //     setEnd(10);
-        // }
-        // // friends.slice(0, end)
-        // setFriendsData(friends.filter((friend: user) => friend.username.startsWith(e.target.value)).slice(0, end));
+    try {
+      const res = await fetch(`http://localhost:3001/api/search?key=${currentSearch}`, {
+        method: "GET",
+      });
+      const result = await res.json();
+      setEnd(10);
+      setFriendsData(result.slice(0, 10));
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Handle the error as needed
     }
+  };
+
+
+const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  setSearch(e.target.value);
+};
+
   return (
-    // <form className="flex items-center">
     <div className="flex flex-row gap-2">
-      <label htmlFor="game search" className="sr-only">
-        Search
-      </label>
-      <div className="relative w-full">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-          <BiSearchAlt className="w-4 h-4 text-gray-500" />
-        </div>
-        <input
-          type="text"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
-          placeholder="Search for Friend"
-          onChange={handleChange}
-          value={search}
-          required
-        />
-      </div>
-      <SearchButton handleChange={handelOnClick}/>
-      </div>
-    // </form>
+            <label htmlFor="game search" className="sr-only">
+              Search
+            </label>
+            <div className="relative w-full">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <BiSearchAlt className="w-4 h-4 text-gray-500" />
+              </div>
+              <input
+                type="text"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
+                placeholder="Search for Friend"
+                onChange={handleChange}
+                value={search}
+                required
+              />
+            </div>
+            <button
+              onClick={handleOnClick}
+              type="button" // Specify the button type
+              className="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-black rounded-lg hover:bg-[#00000097] focus:ring-4 focus:outline-none focus:ring-blue-300"
+            >
+              <svg
+                className="w-4 h-4 me-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+              Search
+            </button>
+          </div>
   );
 };
 
