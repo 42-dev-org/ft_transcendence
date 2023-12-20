8a32460f6bfa6ac249d9a539cb58f0cce64a26e1
mustapha ouarsass <hzan1996@gmail.com>
2023-12-20 16:54:52 +0100

    updated not yet

--- main-project/apps/client/components/chat-main-channel/ListOfUsersChannel.tsx
+++ main-project/apps/client/components/chat-main-channel/ListOfUsersChannel.tsx
@@ -1,34 +1,41 @@
 import Image from 'next/image'
 import React from 'react'
+import MenuItem from '../Menu-chat'
+import { IoMdMore } from 'react-icons/io'
 
 type usersInChannal = {
     name: string,
     url: string,
 }
 
-function ListOfUsersChannal({ name, url}): JSX.Element {
+function ListOfUsersChannal({ name, url }): JSX.Element {
 
+    const onActionClicked = (action: string) => {
+        switch (action) {
+            case 'ban': 
+            break;
+        }
+    }
     return (
         <div className='flex flex-row relative h-16 hover:bg-[#1B1B1B] w-full'>
-            <div className=''>
-                <Image width={40} height={40} className='rounded-full absolute  left-3 bottom-2 ' alt='zakaria' src={url} />
-            </div>
             <div className='flex flex-row w-full justify-between'>
-
-                <div className='flex flex-col ml-16  py-2'>
-                    <span className=' text-[#F5F5F5] text-md font-mono'>
+                <div><Image width={40} height={40} className='rounded-full absolute  left-3 bottom-2 ' alt='zakaria' src={url} /></div>
+                <div className='flex flex-col py-2'>
+                    <span className=' text-[#F5F5F5] text-md font-mono text-center'>
                         {name}
                     </span>
-                    {/* <span className='  text-sm flex justify-star items-center  font-medium text-[#707991]'>
-                        You:
-                        {msg}
-                    </span> */}
                 </div>
-                {/* <div className='flex  items-start mt-1'>
-                    <span className=' text-[#707991]  mt-2 justify-end text-xs'>
-                        {time}
-                    </span>
-                </div> */}
+                <div className='flex  items-start mt-1'>
+                    <div className='text-[#707991]  mt-2 justify-end text-xs'>
+                        <MenuItem iconBtn={
+                            <IoMdMore size={24} color="gray" />
+                        } >
+                            {
+                                ['Mute', 'Ban', 'kick'].map(action => <button className=" hover:bg-[#B2F35F] rounded-md" onClick={onActionClicked.bind(null, action.toLowerCase())} >{action}</button>)
+                            }
+                        </MenuItem>
+                    </div>
+                </div>
             </div>
         </div>
     )
