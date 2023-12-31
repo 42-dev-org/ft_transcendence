11d3926b610d713a5ec0d288131f19b776a2ab67
XasTrO <team.abn33@gmail.com>
2023-12-31 19:17:01 +0200

    fixed responsive

--- main-project/apps/client/components/game-home/play-random/play-random.tsx
+++ main-project/apps/client/components/game-home/play-random/play-random.tsx
@@ -11,43 +11,39 @@ import { useAppSelector } from "../../../store/store";
 
 
 
+
 export default function PlayRandom(): JSX.Element {
     const [timer, setTimer] = useState(60); // Initial timer value in seconds
     const user = useAppSelector(s => s.user.user)
 
     // Function to decrement the timer every second
-    const decrementTimer = () => {
-        if (timer > 0) {
-            setTimer((prevTimer) => prevTimer - 1);
-        }
-    };
-
     useEffect(() => {
-        const interval = setInterval(decrementTimer, 1000);
-
-        // Clear the interval when the component is unmounted
-        return () => clearInterval(interval);
-    }, [timer]);
+        //emit join random mode
+        return () => {
+            //emit leave random mode 
+        }
+    }, [])
+   
 
     return (
         <div className="flex flex-col items-center">
-            <div className="flex flex-col md:flex-row items-center md:items-start justify-between">
-                <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 mb-4 mt-10">
+            <div className="w-full px-0 lg:px-12 flex flex-col md:flex-row items-center justify-between ">
+                <div className=" w-40 mb-4 mt-10 overflow-hidden">
                     <div className="w-full h-40 flex rounded-lg border border-[#B2F35F] overflow-hidden flex-col">
                         <Image
-                            className="w-full h-40 object-cover max-w-full"
+                            className="w-full h-32 object-cover"
                             alt="profile"
                             src={user?.profileImage || ""}
                             width={300}
                             height={300}
                         />
-                        <div className="bg-[#1c1e21] w-full h-1/4 space-y-2 flex py-2 px-5 flex-col items-center">
+                        <div className="bg-[#1c1e21] w-full h-8 flex  flex-col items-center">
                             <span className="text-[#e4e6eb] capitalize">{user?.login || ""}</span>
                         </div>
                     </div>
                 </div>
 
-                <div className="flex flex-row items-center justify-center w-20 h-20 rounded-full border border-[#B2F35F]">
+                <div className="flex items-center justify-center w-20 h-20 rounded-full border border-[#B2F35F]">
                     <Image
                         className="w-20 h-20 object-cover rounded-full"
                         alt="profile"
@@ -55,28 +51,26 @@ export default function PlayRandom(): JSX.Element {
                     />
                 </div>
 
-                <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 mb-4 mt-10">
+                <div className="w-40 mb-4 mt-10 overflow-hidden">
                     <div className="w-full h-40 flex rounded-lg border border-[#B2F35F] overflow-hidden flex-col">
                         <Image
-                            className="w-full h-40 object-cover max-w-full"
+                            className="w-full h-32 object-cover"
                             alt="profile"
                             src={Avatar}
                         />
-                        <div className="bg-[#1c1e21] w-full h-1/4 space-y-2 flex py-2 px-5 flex-col items-center">
+                        <div className="bg-[#1c1e21] w-full h-8 flex flex-col items-center">
                             <span className="text-[#e4e6eb] capitalize">eheh</span>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div className="w-full text-center">
+            {/* <div className="w-full text-center">
                 <p className="text-[#e4e6eb]">
                     Queue: 2/20 waiting in the Queue
                 </p>
-                <span className="text-[#e4e6eb]">
-                    Time remaining: {timer} seconds
-                </span>
-            </div>
+                
+            </div> */}
         </div>
 
 
--- main-project/apps/client/layouts/game-home-main/game-home-main.tsx
+++ main-project/apps/client/layouts/game-home-main/game-home-main.tsx
@@ -12,7 +12,6 @@ import Image from "next/image";
 import GameManualDialog from "../../components/game-home/game-manual/game-manual";
 import InviteFriend from "../../components/game-home/invite-friend/invite-friend";
 import PlayRandom from "../../components/game-home/play-random/play-random";
-import { useAppSelector } from "../../store/store";
 
 
 
@@ -21,12 +20,10 @@ export default function HomeGameMain(): JSX.Element {
   const [playing, setPlaying] = useState(false);
   const [showManual, setShowManual] = useState(false);
   const [typeGame, setTypeGame] = useState<string | null>(null)
-  const [levelGame, setLevelGame] = useState<string | null>(null)
+  // const [levelGame, setLevelGame] = useState<string | null>(null)
   const [selected, setSelected] = useState<boolean>(false);
 
-  const good: boolean = (levelGame != null && typeGame != null);
-
-  const user = useAppSelector(s => s.user.user)
+  const good: boolean = (typeGame != null);
 
 
   const handleManualClick = () => {
@@ -37,7 +34,6 @@ export default function HomeGameMain(): JSX.Element {
     setShowManual(false);
   };
 
-  
 
 
   useEffect(() => {
@@ -49,15 +45,8 @@ export default function HomeGameMain(): JSX.Element {
   return (
     <div className="flex flex-col w-full sm:w-11/12 md:w-3/4 lg:w-2/3 xl:w-1/2 mx-auto max-h-screen h-full rounded-xl bg-[#1B1B1B] text-white text-center pt-8">
       <div className="flex flex-col items-center">
-        <Image
-          src={user?.profileImage || ""}
-          alt="User Avatar"
-          className="rounded-full w-20 h-20 mb-4"
-          width={300}
-          height={300}
-        />
-        <p className="text-lg font-bold mb-2">{user?.login || ""}</p>
-        <p className="text-white">Welcome! Start your game here.</p>
+
+      <p className="text-white text-2xl">Welcome! Start your game here.</p>
       </div>
       <div
         className=" py-2 px-6 rounded-lg mt-4 cursor-pointer bg-[#e5e7eb] hover:opacity-90 text-black font-bold "
@@ -69,7 +58,7 @@ export default function HomeGameMain(): JSX.Element {
       <div className="w-full mx-auto h-fit rounded-xl bg-[#1B1B1B] text-white text-center px-2 py-2">
             {!selected && (
           <div className="flex flex-col md:flex-col">
-            <OptionList data={levelsData} setLevelOrType={setLevelGame} />
+            {/* <OptionList data={levelsData} setLevelOrType={setLevelGame} /> */}
             <OptionList data={gamesData} setLevelOrType={setTypeGame} />
           </div>
         )}
@@ -77,22 +66,20 @@ export default function HomeGameMain(): JSX.Element {
           <div className="flex flex-col md:flex-col">
             {typeGame === "friend" && <InviteFriend />}
             {typeGame === "random" && <PlayRandom />}
-            {typeGame === "bot" && <div>bb</div>}
           </div>
         )}
         <button
           className={`w-full mb-10 ${good
-              ? 'bg-[#B2F35F] hover:opacity-90  cursor-pointer'
-              : ' bg-[#DAF7A6] cursor-default'
+              ? 'bg-[#B2F35F] hover:opacity-90 cursor-pointer'
+              : ' bg-[#e5e7eb] cursor-default'
             } text-black py-3 px-6 rounded-lg mt-8 cursor-pointer  font-bold`}
           onClick={() => {
             setSelected(!selected);
             if (selected == true) {
-              setLevelGame(null);
               setTypeGame(null);
             }
           }}
-          disabled={levelGame != null && typeGame != null ? false : true}
+          disabled={typeGame != null ? false : true}
         >
           {selected ? "Cancel" : "Next"}
         </button>
