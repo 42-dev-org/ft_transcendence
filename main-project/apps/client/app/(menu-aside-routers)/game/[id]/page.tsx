c6e5a3da36e6db26744844eb24be60b42f2130f8
amounach <mounach-@hotmail.com>
2024-01-07 18:31:57 +0100

    feat: extract id from URL params in InternalGamePage

--- main-project/apps/client/app/(menu-aside-routers)/game/[id]/page.tsx
+++ main-project/apps/client/app/(menu-aside-routers)/game/[id]/page.tsx
@@ -2,8 +2,8 @@
 import { useParams } from "next/navigation";
 import SingleGameContainer from "../../../../containers/single-game/single-game-container";
 
-export default function InternalGamePage(): JSX.Element {
+export default function InternalGamePage({ params }: { params: { id: string } }): JSX.Element {
   const { id } = useParams();
-
-  return <SingleGameContainer _gameid={id as string} />;
+console.log("param: ", params.id)
+  return <SingleGameContainer _gameid={params.id as string} />;
 }
